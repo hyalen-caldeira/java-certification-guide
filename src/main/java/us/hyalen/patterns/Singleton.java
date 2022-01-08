df542cb5bdd5018a6929907768c7a13b1a4acaeb
Hyalen Caldeira <hyalen.moreira@gmail.com>
2022-01-08 10:23:09 -0500

    Added singleton pattern

---
+++ src/main/java/us/hyalen/patterns/Singleton.java
@@ -0,0 +1,30 @@
+package us.hyalen.patterns;
+
+public class Singleton {
+    // Uncomment in case you will use the synchronized version
+    // private static volatile Singleton instance;
+    private static Singleton instance;
+
+    private Singleton() {}
+
+    // public static synchronized Singleton getInstance() {
+    public static Singleton getInstance() {
+        if (instance == null)
+            instance = new Singleton();
+
+        return instance;
+    }
+
+    /*
+    // Synchronized version with better performance
+    public static Singleton getInstance() {
+        if (instance == null)
+            synchronized (Singleton.class) {
+                if (instance == null)
+                    instance = new Singleton();
+            }
+
+        return instance;
+    }
+    */
+}
